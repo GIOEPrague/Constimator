604251f0cd0f7917f9c5a2da4b77180825c99451
Jan Linhart <jan.linhart@easysoftware.cz>
2012-06-27 23:22:09 +0200

    Loading layout files
    Signed-off-by: Jan Linhart <jan.linhart@easysoftware.cz>

---
+++ helper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package	Joomla.Site
+ * @subpackage	mod_escope_currency_converter
+ * @copyright	Copyright (C) 2005 - 2012 Jan Linhart aka escope.cz. All rights reserved.
+ * @license	GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+class modEscopeCurrencyConverterHelper
+{
+
+}
---
+++ tmpl/default.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package	Joomla.Site
+ * @subpackage	mod_escope_currency_converter
+ * @copyright	Copyright (C) Jan Linhart aka escope.cz. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
